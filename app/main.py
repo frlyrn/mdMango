aca58e541100c4c5d987b09eea58c238f7ed269a
fauraalyriana <fauraalyrianas@gmail.com>
2025-06-17 00:02:04 +0700

    edit port

--- app/main.py
+++ app/main.py
@@ -70,4 +70,6 @@ def detect():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
